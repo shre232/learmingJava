d870cbd7812776482487c6d46ae44a3f5ee264c8
Shre232 <you@example.com>
2022-06-07 22:04:59 +0530

    Initial commit

---
+++ src/com/learnjavaforcertification/Shre_java_1.java
@@ -0,0 +1,14 @@
+/*
+*Learning java for 1zo_808 certification
+* developer: Shreyash Gaikwad
+* Goal:MAANG F in pune
+ */
+package com.learnjavaforcertification;
+
+public class Shre_java_1 {
+    public static void main(String[] args) {
+        //first java code
+        System.out.println("Hello World!");
+
+    }
+}
